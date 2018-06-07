03a5728b85ec5c55d1194ef189903e5d5eb0e612
JannisLuechtefeld <jannis.luechtefeld@yahoo.de>
2018-06-07 16:00:14 +0200

    registrieren

--- www/register.php
+++ www/register.php
@@ -7,14 +7,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Registrieren</title>
+    <link href="../lib/bootstrap-3.3.5-dist/css/bootstrap.css">
 </head>
 <body>
-    <form>
-        <label>Username: <input type="text" name="username"></label>
-        <label>Email: <input type="email" name="email"></label>
-        <label>Passwort:<input type="password" name="passwort"> </label>
-        <label>Passwort wiederholen:<input type="password" name="passwort"> </label>
-        <button>Register</button>
+<h1 class="h1">Register</h1>
+<div class="form-group">
+    <form method="post">
+        <div class="form-group">
+        <label>Username: <input type="text" name="username" class="form-control"></label>
+        </div>
+        <div class="form-group">
+        <label>Email: <input type="email" name="email" class="form-control"></label>
+        </div>
+        <label>Password:<input type="password" name="password" class="form-control"> </label>
+        <label>Password:<input type="password" name="password2" class="form-controlS"> </label>
+        <button class="btn btn-primary">Register</button>
     </form>
+</div>
 </body>
 </html>
