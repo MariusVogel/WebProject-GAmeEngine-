50a2422522a58073069e9c7508cdea170ce427d2
JannisLuechtefeld <jannis.luechtefeld@yahoo.de>
2018-06-14 13:56:52 +0200

    registrieren

--- www/register.php
+++ www/register.php
@@ -7,22 +7,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Registrieren</title>
-    <link href="../lib/bootstrap-3.3.5-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../lib/bootstrap/css/cosmo.css">
 </head>
 <body>
-<h1 class="h1">Register</h1>
-<div class="form-group">
-    <form method="post">
-        <div class="form-group">
-        <label>Username: <input type="text" name="username" class="form-control"></label>
-        </div>
-        <div class="form-group">
-        <label>Email: <input type="email" name="email" class="form-control"></label>
-        </div>
-        <label>Password:<input type="password" name="password" class="form-control"> </label>
-        <label>Password:<input type="password" name="password2" class="form-controlS"> </label>
-        <button class="btn btn-primary">Register</button>
-    </form>
+<div class="jumbotron">
+    <h1 class="h1">Register</h1>
+    <div class="form-group row">
+        <form method="post">
+            <label for="usern">Username: <input type="text" name="username" class="form-control" id="usern"></label>
+            <label for="mail">Email: <input type="email" name="email" class="form-control" id="mail"></label>
+            <label for="pw1">Password:<input type="password" name="password" class="form-control" id="pw1"> </label>
+            <label for="pw2">Password:<input type="password" name="password2" class="form-control" id="pw2"> </label>
+            <button class="btn btn-primary">Register</button>
+        </form>
+    </div>
 </div>
 </body>
 </html>
