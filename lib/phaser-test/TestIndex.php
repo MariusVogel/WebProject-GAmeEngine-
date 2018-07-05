9b961968fa40b2d5fc9928efa73f8b5fbd9e346e
hanna2802 <ho2802.HO@gmail.com>
2018-07-05 14:43:44 +0200

    start screen

--- lib/phaser-test/TestIndex.php
+++ lib/phaser-test/TestIndex.php
@@ -14,10 +14,13 @@
     <script src="//cdn.jsdelivr.net/npm/phaser@3.1.1/dist/phaser.js"></script>
     <script src="TestGame.js"></script>
     <link rel="stylesheet" href="../bootstrap/css/cosmo.css">
+    <link rel="stylesheet" href="test.css">
 
 
 </head>
 <body>
     <?php echo Help::getNavbar()?>
+    <button id="startbutton" onclick="starteSpiel()" class="btn ">START</button>
+
 </body>
 </html>
